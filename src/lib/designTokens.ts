c55fd12d41be2387153b6e63c3ea2de5dd9d0a03
Thefosaken <emonbeifoefosasere@gmail.com>
2026-05-15 20:30:32 +0100

    Increase topic font size by 0.3pt

--- src/lib/designTokens.ts
+++ src/lib/designTokens.ts
@@ -27,7 +27,7 @@ export const LAYOUTS = {
     leftPadding: 55,
     rightPadding: 55,
     logoToCityGap: 6,
-    topicFontSize: 19.3,
+    topicFontSize: 19.6,
     cityFontSize: 15.17,
   },
   landscape: {
@@ -44,7 +44,7 @@ export const LAYOUTS = {
     leftPadding: 55,
     rightPadding: 55,
     logoToCityGap: 6,
-    topicFontSize: 19.3,
+    topicFontSize: 19.6,
     cityFontSize: 15.17,
   },
 } as const;
